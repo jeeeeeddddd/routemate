2c783a7d908b81695718b2404b8c361b83d0808f
John Elison Duero <johnelison_duero@yahoo.com>
2023-05-05 18:19:03 +0800

    Updated ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,10 +3,11 @@ import { useSearchParams, useLocation } from "react-router-dom";
 export const ProductList = () => {
 
   const [searchParams] = useSearchParams();
-  //const location = useLocation();
-
-  console.log(location);
   //console.log(searchParams.get("keyword"));
+  
+  const location = useLocation();
+  //console.log(location);
+  
 
     return (
       <div className="component">ProductList</div>
